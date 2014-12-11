78493589cba64354c14a7e9afc943b99721d38fe
kisna72 <kisna72@gmail.com>
2014-12-10 21:51:44 -0600

    update the gh-pages branch

--- js/ang_routr.js
+++ js/ang_routr.js
@@ -57,6 +57,29 @@
         $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
     });
 
+    exoticApp.controller('DropdownCtrl', function ($scope, $log) {
+      $scope.items = [
+        'The first choice!',
+        'And another choice for you.',
+        'but wait! A third!'
+      ];
+
+      $scope.status = {
+        isopen: false
+      };
+
+      $scope.toggled = function(open) {
+        $log.log('Dropdown is now: ', open);
+        console.log('Yeah')
+      };
+
+      $scope.toggleDropdown = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.status.isopen = !$scope.status.isopen;
+      };
+    });
+
 
 // angular.module('project', ['ngRoute'])
  
