83fbafd9be74c6d9f6b007e256d86c26450a4fd1
kisna72 <kisna72@gmail.com>
2014-12-14 14:05:46 -0600

    changed folder stducture to make the flask server compatible. In addition, removed some features to ensure the fixed menu links are clickable

--- flask_server.py
+++ flask_server.py
@@ -2,16 +2,34 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 app.debug = True
-app.template_folder = ""
-app.static_path=""
-app.static_folder =""
-
+app.template_folder = r"C:\Users\Krishna\Desktop\glenn-website\templates"
+#app.static_path=r"C:\Users\Krishna\Desktop\glenn-website\templates"
+app.static_folder = r"C:\Users\Krishna\Desktop\glenn-website\templates\static"
+#app.static_url_path = r"C:\Users\Krishna\Desktop\glenn-website\templates"
+static_path = app.static_folder
+static_url = app.static_url_path
+print(static_path, static_url)
 
 
 @app.route("/")
 def hello():
     return render_template('index.html')
     #return "Hello World!"
+@app.route("/pages/home.html")
+def home_page():
+	return render_template('pages/home.html')
+
+@app.route("/pages/about.html")
+def about_page():
+	return render_template('pages/about.html')
+
+@app.route("/pages/services.html")
+def services_page():
+	return render_template('pages/services.html')
+
+@app.route("/pages/location.html")
+def location_page():
+	return render_template('pages/location.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
